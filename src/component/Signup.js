1c9b646eb7cc968d939f009d4e4c63a08c55a90a
irfan-ahmed24 <irfanahmed99989@gmail.com>
2025-01-15 21:44:24 +0600

    sign up validation form

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -1,29 +1,74 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
 
 import "./Signup.css"
 
 export default function Signup() {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            phone: '',
+            email: '',
+            password: '',
+            Confirm: ''
+        },
+        onSubmit: (value, { resetForm }) => {
+            console.log(value)
+            resetForm(value = '')
+        }
+    })
     return (
         <>
             <div className='container'>
                 <div className='bg'>
                     <h1>Sign Up</h1>
-                    <form>
+                    <form onSubmit={formik.handleSubmit}>
                         <div className='form-group'>
-                            <input type='text' className='form-control' id='name' placeholder='Username..' required />
+                            <input type='text'
+                                className='form-control'
+                                id='name'
+                                placeholder='Username..'
+                                onChange={formik.handleChange}
+                                value={formik.values.name}
+                                required
+                            />
                         </div>
                         <div className='form-group'>
-                            <input type='number' className='form-control' id='email' placeholder='Enter your phone number..' required />
+                            <input type='number'
+                                className='form-control'
+                                id='phone'
+                                placeholder='Enter your phone number..'
+                                onChange={formik.handleChange}
+                                value={formik.values.phone}
+                                required />
                         </div>
                         <div className='form-group'>
-                            <input type='email' className='form-control' id='email' placeholder='Enter your email..' required />
+                            <input type='email'
+                                className='form-control'
+                                id='email'
+                                placeholder='Enter your email..'
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                                required />
                         </div>
                         <div className='form-group'>
-                            <input type='password' className='form-control' id='password' placeholder='Enter your password..' required />
+                            <input type='password'
+                                className='form-control'
+                                id='password'
+                                placeholder='Enter your password..'
+                                onChange={formik.handleChange}
+                                value={formik.values.password}
+                                required />
                         </div>
                         <div className='form-group'>
-                            <input type='password' className='form-control' id='Confirm' placeholder='Confirm password..' required />
+                            <input type='password'
+                                className='form-control'
+                                id='Confirm'
+                                placeholder='Confirm password..'
+                                onChange={formik.handleChange}
+                                value={formik.values.Confirm}
+                                required />
                         </div>
                         <button type='submit' className='btn btn-primary'>Sign Up</button>
                         <p>Or</p>
