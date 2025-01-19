beee847c86bb8ce0f27dba8a1af4ed382322bd21
irfan-ahmed24 <irfanahmed99989@gmail.com>
2025-01-19 14:11:15 +0600

    login section

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
 
 
 import './Login.css'
 
 export default function Login() {
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            Password: ''
+        },
+        onSubmit: (values, { resetForm }) => {
+            console.log(values)
+            resetForm(values = '')
+        }
+    })
     return (
         <div className='loginconainer'>
             <div className='loginbg'>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={formik.handleSubmit}>
                     <div className='from-group'>
                         <input type="text"
                             placeholder="Username"
                             name='username'
                             className='form-control'
                             id='username'
+                            value={formik.values.username}
+                            onChange={formik.handleChange}
                             required />
                     </div>
                     <div className='form-group'>
@@ -24,6 +37,8 @@ export default function Login() {
                             name='Password'
                             className='form-control'
                             id='Password'
+                            value={formik.values.Password}
+                            onChange={formik.handleChange}
                             required />
                     </div>
                     <button type="submit" className='loginbtn'>Login</button>
